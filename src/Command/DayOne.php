e101dc6206077c85df0ed1b7b2b62057b30836c2
Sander Verkuil <sander@legionelladossier.com>
2024-01-08 00:43:50 +0100

    Resolved part 2 of day one

--- src/Command/DayOne.php
+++ src/Command/DayOne.php
@@ -12,40 +12,80 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class DayOne extends Command
 {
+    private const MAPPING = [
+        1 => 'one',
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
+    ];
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $contents = file_get_contents(__DIR__ . '/../../input/d1.txt');
 
         $lines = explode(PHP_EOL, $contents);
 
-        $characters = array_map(
-            static fn(string $line) => str_split($line),
-            $lines
-        );
-
-        $integers = array_map(
-            static fn(array $line) => array_filter(
-                $line,
-                static fn(string $char) => is_numeric($char)
-            ),
-            $characters
-        );
-
-        $first = static fn (array $element) => reset($element);
-        $last = static fn(array $element) => array_pop($element);
+        $first = fn (string $element) => $this->firstElement($element);
+        $last = fn(string $element) => $this->lastElement($element);
 
         $coords = array_map(
             static fn($line) => [(int)$first($line), (int) $last($line)],
-            $integers
+            $lines
         );
 
-        dump(array_sum(
+        $sum = array_sum(
             array_map(
-            static fn(array $coord) => (int) ($coord[0] . $coord[1]),
+                static fn(array $coord) => (int) ($coord[0] . $coord[1]),
                 $coords
             )
-        ));
+        );
+
+        $output->write('The sum is: ' . $sum . PHP_EOL);
 
         return self::SUCCESS;
+    }
+
+    private function firstElement(string $line)
+    {
+        $index = 0;
+        do {
+            $character = $line[$index++];
+
+            if (is_numeric($character)) {
+                return $character;
+            }
+
+            foreach(self::MAPPING as $number => $value) {
+                if (str_starts_with($value, $character)) {
+                    if (substr($line, $index-1, strlen($value)) === $value) {
+                        return $number;
+                    }
+                }
+            }
+        } while ($index < strlen($line));
+    }
+    private function lastElement(string $line)
+    {
+        $index = strlen($line);
+        do {
+            $character = $line[--$index];
+            if (is_numeric($character)) {
+                return $character;
+            }
+
+            foreach(self::MAPPING as $number => $value) {
+                if (str_ends_with($value, $character)) {
+                    if (substr($line, $index+1-strlen($value), strlen($value)) === $value) {
+                        return $number;
+                    }
+                }
+            }
+
+        } while ($index > 0);
     }
 }
